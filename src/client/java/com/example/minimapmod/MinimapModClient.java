447f54550b362003a758b5019fffa3038a56278d
Divyanshproo <Divyansh10298@gmail.com>
2025-08-07 00:05:45 +0530

    Update MinimapModClient.java

--- src/client/java/com/example/minimapmod/MinimapModClient.java
+++ src/client/java/com/example/minimapmod/MinimapModClient.java
@@ -1,12 +1,11 @@
 package com.example.minimapmod;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 
 public class MinimapModClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         KeyBindings.register();
-        HudRenderCallback.EVENT.register(new MinimapRenderer());
+        MinimapRenderer.init(); // This is the correct place for it
     }
 }
